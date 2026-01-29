39cfabd4b826fcf6262487c785aa04925f7be5a1
sunnystew <sunnystew22@gmail.com>
2026-01-29 19:06:58 +0330

    Update initialsetup.sh

--- initialsetup.sh
+++ initialsetup.sh
@@ -1,18 +1,19 @@
 echo "Installing packages..."
 sudo apt update
 sudo apt install qemu-kvm virtinst libvirt-clients bridge-utils libvirt-daemon-system tigervnc-standalone-server dwm aqemu -y > installlogs.txt
-echo "Packages installed. Now downloading Tiny10 image..."
-cd /
-sudo wget -O Tiny10_x64_HDA.img https://files.mollomm1.dev/api/public/dl/eHWIyWhJ
-echo "Tiny10 image downloaded. Now downloading NoVNC..."
+echo "Packages installed."
+echo "Now downloading NoVNC..."
 sudo git clone https://github.com/novnc/noVNC
-echo "NoVNC downloaded. Now downloading Tiny10 config..."
+echo "NoVNC downloaded."
 sudo mkdir /root/.aqemu
-sudo wget -O /root/.aqemu/Tiny_10_x64.aqemu https://files.mollomm1.dev/api/public/dl/fCJ-7xlS
-echo "Tiny10 config downloaded. Now running VNC server..."
+cd ~
+mkdir tiny10
+cd tiny10
+sudo qemu-img create hdd.img 26G
+sudo wget https://github.com/L36D/Tiny10/releases/download/v1/Tiny10.iso
+echo "Virtual HDD with size of 26 GB made. Tiny10 ISO downloaded."
+qemu-system-x86_64 --enable-kvm -m 3G -smp 2 -pflash /usr/share/OVMF/OVMF_CODE.fd --hdd hdd.img --cdrom Tiny10.iso
+echo "VM started. Now running VNC server..."
 sudo vncserver -SecurityType None -xstartup "dwm" -rfbport 5900 && sudo /noVNC/utils/novnc_proxy --vnc 127.0.0.1:5900 --listen localhost:6080
 echo "VNC server is running."
-echo "After opening the NoVNC client, hit Alt+P and type aqemu."
-echo "AQEMU will warn you that you are running it as root, just click no and ignore."
-echo "In the Find QEMU setup step be sure to click the search button or else it will break."
-echo "You can now start the VM."
+echo "Please start the installation of Windows."
